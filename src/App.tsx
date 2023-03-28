61a1e93df8e43f1ad9e8f1cd71f999431217a2b1
Allex372 <alexalex372.aa@gmail.com>
2023-03-28 09:38:39 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import { BaseLayout } from './layouts';
 
@@ -9,15 +9,18 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <BaseLayout>
-        <Routes>
-          <Route path="/" element={<GreatingPage />} />
-          <Route path="/weapones-categories" element={<WeaponesCategory />} />
-          <Route path="/pistols-list" element={<PistolsList />} />
-        </Routes>
-      </BaseLayout>
-    </Router>
+    <div>
+      hello
+    </div>
+    // <Router>
+    //   <BaseLayout>
+    //     <Routes>
+    //       <Route path="/" element={<GreatingPage />} />
+    //       <Route path="/weapones-categories" element={<WeaponesCategory />} />
+    //       <Route path="/pistols-list" element={<PistolsList />} />
+    //     </Routes>
+    //   </BaseLayout>
+    // </Router>
   );
 }
 
